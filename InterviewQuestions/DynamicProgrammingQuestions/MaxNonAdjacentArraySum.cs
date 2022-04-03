832e2d32e7ed1c816104f5435834759ee7ea3803
anjalimnnit <anjalishah.mnnit@gmail.com>
2022-04-03 20:57:54 +0530

    Alternate max sum aarray

---
+++ InterviewQuestions/DynamicProgrammingQuestions/MaxNonAdjacentArraySum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgrammingQuestions
+{
+    class MaxNonAdjacentArraySum
+    {
+        public static int MaxSum(int[] arr, int n)
+        {
+            int[] maxIncludedTillThisElement = new int[n];
+            maxIncludedTillThisElement[0] = arr[0];
+            int[] maxExcludedTillThisElement = new int[n];
+            maxExcludedTillThisElement[0] = 0;
+            int maxGlobal = arr[0];
+            for(int i =1;i<n;i++)
+            {
+                maxIncludedTillThisElement[i] = Math.Max(arr[i], maxExcludedTillThisElement[i - 1] + arr[i]);
+                maxExcludedTillThisElement[i] = Math.Max(maxIncludedTillThisElement[i - 1], maxExcludedTillThisElement[i - 1]);
+            }
+            int max = Math.Max(maxIncludedTillThisElement[n - 1], maxExcludedTillThisElement[n - 1]);
+            return maxGlobal < max ? max : maxGlobal;
+        }
+    }
+}
--- InterviewQuestions/DynamicProgrammingQuestions/Program.cs
+++ InterviewQuestions/DynamicProgrammingQuestions/Program.cs
@@ -4,7 +4,7 @@ namespace DynamicProgrammingQuestions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int[] val = new int[] { 60, 100, 120 };
             int[] wt = new int[] { 10, 20, 30 };
@@ -12,6 +12,8 @@ namespace DynamicProgrammingQuestions
             int n = val.Length;
             Knapsack01 ks = new Knapsack01();
             Console.WriteLine(ks.KnapSack(W, wt, val, n));
+            int[] array = new int[5] { -2, 1, 3, -4, 5 };
+            Console.WriteLine($"Maximum adjacent array sum is:{ MaxNonAdjacentArraySum.MaxSum(array, array.Length)}");
         }
     }
 }
