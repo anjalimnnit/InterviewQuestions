9413e836bf66cc8d459af3b6fc3cd4f9dc0c6b2b
anjalimnnit <anjalishah.mnnit@gmail.com>
2022-05-05 17:22:59 +0530

    Decorator pattern added

---
+++ InterviewQuestions/DesignPatterns/ConcreteDecorator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class BasicAccessories : CarAccessoriesDecorator
+    {
+        public BasicAccessories(ICar aCar)
+        : base(aCar)
+        {
+
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + ",Basic Accessories Package";
+
+        }
+
+        public override double GetCost()
+        {
+            return base.GetCost() + 2000.0;
+        }
+    }
+
+    /// <summary>   
+    /// Concrete Decorator   
+    /// </summary>   
+    public class AdvancedAccessories : CarAccessoriesDecorator
+    {
+        public AdvancedAccessories(ICar aCar)
+        : base(aCar)
+        {
+
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + ",Advanced Accessories Package";
+        }
+
+        public override double GetCost()
+        {
+            return base.GetCost() + 10000.0;
+        }
+    }
+
+    /// <summary>   
+    /// Concrete Decorator   
+    /// </summary>   
+    public class SportsAccessories : CarAccessoriesDecorator
+    {
+        public SportsAccessories(ICar aCar)
+        : base(aCar)
+        {
+
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + ",Sports Accessories Package";
+        }
+
+        public override double GetCost()
+        {
+            return base.GetCost() + 15000.0;
+        }
+    }
+}
---
+++ InterviewQuestions/DesignPatterns/DecoratorDesignPattern.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    
+        /// <summary>   
+        /// Car Base component   
+        /// </summary>   
+        public interface ICar
+        {
+            string GetDescription();
+            double GetCost();
+        }
+
+        /// <summary>   
+        /// Concrete Car   
+        /// </summary>   
+        public class EconomyCar : ICar
+        {
+            public string GetDescription()
+            {
+                return "Economy Car";
+            }
+
+            public double GetCost()
+            {
+                return 450000.0;
+            }
+        }
+
+        /// <summary>   
+        /// Concrete Car   
+        /// </summary>   
+        public class DeluxCar : ICar
+        {
+            public string GetDescription()
+            {
+                return "Delux Car";
+            }
+
+            public double GetCost()
+            {
+                return 750000.0;
+            }
+        }
+
+        /// <summary>   
+        /// Concrete Car   
+        /// </summary>   
+        public class LuxuryCar : ICar
+        {
+            public string GetDescription()
+            {
+                return "Luxury Car";
+            }
+
+            public double GetCost()
+            {
+                return 1000000.0;
+            }
+        }
+    }
